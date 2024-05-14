1a7db675658f58d931dec9d532abb8a6a9e0d6e1
Raphael Nunes <52461782+phaaael@users.noreply.github.com>
2024-05-13 23:51:50 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import pywhatkit as kit
+import time
+
+def ler_contatos(arquivo):
+    with open(arquivo, 'r') as f:
+        contatos = f.read().splitlines()
+    return contatos
+
+def ler_mensagem(arquivo):
+    with open(arquivo, 'r') as f:
+        mensagem = f.read()
+    return mensagem
+
+arquivo_contatos = 'contatos.txt'
+arquivo_mensagem = 'mensagem.txt'
+
+contatos = ler_contatos(arquivo_contatos)
+mensagem = ler_mensagem(arquivo_mensagem)
+
+def enviar_mensagem(contatos, mensagem):
+    for contato in contatos:
+        kit.sendwhatmsg(contato, mensagem, time.localtime().tm_hour, time.localtime().tm_min + 2)
+        time.sleep(20)
+
+enviar_mensagem(contatos, mensagem)
